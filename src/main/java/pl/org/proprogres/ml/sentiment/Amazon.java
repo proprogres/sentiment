64002e717508567547285770da1e487962e08811
LA <albrzykowski@gmail.com>
2016-12-15 21:56:17 +0100

    Adding some pipeline steps

--- src/main/java/pl/org/proprogres/ml/sentiment/Amazon.java
+++ src/main/java/pl/org/proprogres/ml/sentiment/Amazon.java
@@ -4,6 +4,7 @@ package pl.org.proprogres.ml.sentiment;
 import org.apache.spark.ml.Pipeline;
 
 import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.ml.PipelineStage;
 import org.apache.spark.ml.classification.NaiveBayes;  
 import org.apache.spark.ml.classification.NaiveBayesModel;
 import org.apache.spark.ml.feature.RegexTokenizer;
@@ -13,11 +14,14 @@ import org.apache.spark.ml.feature.HashingTF;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.feature.StringIndexer;
 import org.apache.spark.ml.feature.IndexToString;
+import org.apache.spark.ml.feature.Tokenizer;
+
 //import org.apache.spark.ml.tuning.{ParamGridBuilder, CrossValidator}  
 import org.apache.spark.ml.tuning.ParamGridBuilder;
 import org.apache.spark.ml.tuning.CrossValidator;
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator;  
 import org.apache.spark.mllib.util.MLUtils;
+
 
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.Dataset;  
@@ -34,7 +38,7 @@ public class Amazon {
 	    JavaSparkContext sc = new JavaSparkContext(conf);
 	    SQLContext sqlContext = new SQLContext(sc);
 	    
-		String reviews = "/home/la/Downloads/Amazon_Instant_Video_5.json";
+		String reviews = "/home/la/Amazon_Instant_Video_5.json";
 		
 		Dataset<Row> reviewsDs = sqlContext.read().json(reviews);
 		reviewsDs.registerTempTable("reviews");
@@ -49,8 +53,33 @@ public class Amazon {
 		reviewsDF.persist(org.apache.spark.storage.StorageLevel.MEMORY_AND_DISK());
 		
 		reviewsDF.groupBy("label").count().orderBy("label").show();
-		// reviewsDs.groupBy("overall").count().orderBy("overall").show();
 		
-		//training = reviewsDs.filter(reviewsDs("rowNumber") <= 15000).select("text","label")
+		Tokenizer tokenizer = new Tokenizer()
+		  .setInputCol("text")
+		  .setOutputCol("words");
+		
+		StopWordsRemover remover = new StopWordsRemover()
+		  .setInputCol(tokenizer.getOutputCol())
+		  .setOutputCol("filtered");
+		  
+		NGram ngram3 = new NGram()
+		  .setInputCol(remover.getOutputCol())
+		  .setN(3);
+		  
+		Pipeline pipeline = new Pipeline()
+      .setStages(new PipelineStage[] {tokenizer, remover, ngram3});
+		  
+    Dataset<Row> training = reviewsDF.filter(reviewsDF("rowNumber") <= 15000).select("text","label"); 
+    Dataset<Row> test = reviewsDF.filter(reviewsDF("rowNumber") > 15000).select("text","label");
+
+    
+    PipelineModel model = pipeline.fit(training);
+    
+    Dataset<Row> predictions = model.transform(test);
+    for (Row r : predictions.select("id", "text", "probability", "prediction").collectAsList()) {
+      System.out.println("(" + r.get(0) + ", " + r.get(1) + ") --> prob=" + r.get(2)
+        + ", prediction=" + r.get(3));
+    }
+    
 	}
 }
