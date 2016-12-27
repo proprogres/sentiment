176d62dcf01d0e9dc9db50bafad13e8317c527d2
LA <albrzykowski@gmail.com>
2016-12-27 01:38:39 +0100

    tmp

--- src/main/java/pl/org/proprogres/ml/sentiment/Amazon.java
+++ src/main/java/pl/org/proprogres/ml/sentiment/Amazon.java
@@ -22,6 +22,10 @@ import org.apache.spark.ml.tuning.CrossValidator;
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator;  
 import org.apache.spark.mllib.util.MLUtils;
 
+import org.apache.spark.ml.classification.DecisionTreeClassifier;
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel;
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator;
+
 import static org.apache.spark.sql.functions.col;
 
 
@@ -36,44 +40,104 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.ml.classification.LogisticRegression;
 import org.apache.spark.ml.feature.HashingTF;
 
+import pl.org.proprogres.ml.sentiment.transformer.Stemmer;
+
+import org.apache.spark.sql.types.IntegerType;
+
 
 public class Amazon {
 	public static void main(String[] args) {
 		SparkConf conf = new SparkConf().setAppName("Simple Application");
-	    JavaSparkContext sc = new JavaSparkContext(conf);
-	    sc.setLogLevel("ERROR");
-	    SQLContext sqlContext = new SQLContext(sc);
+    JavaSparkContext sc = new JavaSparkContext(conf);
+    sc.setLogLevel("ERROR");
+    SQLContext sqlContext = new SQLContext(sc);
 	    
-		String reviews = "/home/la/Downloads/Amazon_Instant_Video_5.json";
+		String reviews = "/home/la/training_twitter_by_smiles_287000.csv";
 		
-		Dataset<Row> reviewsDs = sqlContext.read().json(reviews);
+		Dataset<Row> reviewsDs = sqlContext.read()
+      .option("sep", "|")
+      .option("header", true)
+      .csv(reviews);
+    
 		reviewsDs.registerTempTable("reviews");
-	
-		String query = new StringBuilder()
-				.append("SELECT text, label, rowNumber FROM (SELECT ")
-				.append("reviews.overall AS label ,reviews.reviewText AS text ,row_number() OVER (PARTITION BY overall ORDER BY rand()) AS rowNumber")
-				.append(" FROM reviews) reviews WHERE rowNumber <= 1700")
-				.toString();
 		
-		Dataset<Row> reviewsDF = sqlContext.sql(query);
+		Dataset<Row> reviewsDF = sqlContext.sql("SELECT SENTIMENT as label, TEXT as text FROM reviews WHERE SENTIMENT = -1 OR SENTIMENT = 1 ORDER BY rand()");
+    reviewsDF = reviewsDF
+      .withColumn("label", col("label").cast("Double"))
+      //.withColumn("text", col("text"))
+      .select("label", "text");
 		reviewsDF.persist(org.apache.spark.storage.StorageLevel.MEMORY_AND_DISK());
 		
-		reviewsDF.groupBy("label").count().orderBy("label").show();
+		System.out.println(reviewsDF.count());
+		System.out.println("--------------------------------");
+		
+		//reviewsDF.show(100);
 		
 		Tokenizer tokenizer = new Tokenizer()
 				  .setInputCol("text")
 				  .setOutputCol("words");
-		
-		Dataset<Row> tokenizedDF = tokenizer.transform(reviewsDF);
-		// tokenizedDF.show();
+    Dataset<Row> wordsDF = tokenizer.transform(reviewsDF);
 		
 		StopWordsRemover remover = new StopWordsRemover()
 				  .setInputCol(tokenizer.getOutputCol())
 				  .setOutputCol("filtered");
+		Dataset<Row> filteredDF = remover.transform(wordsDF);
+    
+    NGram ngram = new NGram()
+		  .setInputCol(remover.getOutputCol())
+      .setOutputCol("ngrammed")
+		  .setN(1);
+    Dataset<Row> ngramDF = ngram.transform(filteredDF);
+		//ngramDF.show(50);
+    
+		HashingTF hasher = new HashingTF()
+      .setNumFeatures(60000)
+      .setInputCol(ngram.getOutputCol())
+      .setOutputCol("features");
+    Dataset<Row> featuresDF = hasher.transform(ngramDF);
+    // System.out.println("Number of features: " + hasher.numFeatures());
+      
+    NaiveBayes nb = new NaiveBayes();
+		  
+		Pipeline pipeline = new Pipeline()
+      .setStages(new PipelineStage[] {tokenizer, remover, ngram, hasher, nb});
+		  
 		
-		Dataset<Row> stopwordsRemovedDF = remover.transform(tokenizedDF);
-		stopwordsRemovedDF.show();
-		
+		Dataset<Row>[] splits = reviewsDF.randomSplit(new double[]{0.6, 0.4});
+    Dataset<Row> training = splits[0];
+    Dataset<Row> test = splits[1];
+    //training.show(10);
+    //test.show(10);
+    // Dataset<Row> training = reviewsDF.filter(col("rowNumber").lt(3000)); 
+    // Dataset<Row> test = reviewsDF.filter(col("rowNumber").gt(3000));
+
+    
+    PipelineModel model = pipeline.fit(training);
+    
+    Dataset<Row> predictions = model.transform(test);
+    System.out.println(model.toString());
+    
+    //predictions.select("prediction", "label", "features").show(200);
+    MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy");
+    double accuracy = evaluator.evaluate(predictions);
+    System.out.println("Test Error = " + (1.0 - accuracy));
+    
+    // Select (prediction, true label) and compute test error.
+    /*MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy");
+    double accuracy = evaluator.evaluate(predictions);
+    System.out.println("Test Error = " + (1.0 - accuracy));
+
+    DecisionTreeClassificationModel treeModel =
+      (DecisionTreeClassificationModel) (model.stages()[2]);
+    System.out.println("Learned classification tree model:\n" + treeModel.toDebugString());
+    */
+    
 		/* 
 		Tokenizer tokenizer = new Tokenizer()
 		  .setInputCol("text")
--- src/main/java/pl/org/proprogres/ml/sentiment/transformer/Stemmer.java
+++ src/main/java/pl/org/proprogres/ml/sentiment/transformer/Stemmer.java
@@ -1,38 +1,44 @@
 package pl.org.proprogres.ml.sentiment.transformer;
 
-import org.apache.spark.ml.Transformer;
-import org.apache.spark.ml.param.ParamMap;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.types.StructType;
+import org.apache.spark.ml.UnaryTransformer;
+import org.apache.spark.sql.types.ArrayType;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StringType;
+import scala.Function1;
+import java.util.List;
+import java.io.Serializable;
+import org.apache.spark.ml.feature.Tokenizer;
 
-public class Stemmer extends Transformer {
-	private static final long serialVersionUID = 5545470640951989469L;
-    String column;
-    int power = 1;
+public class Stemmer extends UnaryTransformer<String, List<String>, Tokenizer> implements Serializable {
 
-	Stemmer(String column, int power) {
-	    this.column = column;
-	    this.power = power;
-	}
-	
-	
-	@Override
-	public String uid() {
-	   return "CustomTransformer" + serialVersionUID;
-	}
-	
-	@Override
-	public Transformer copy(ParamMap arg0) {
-	   return null;
-	}
-	
-	@Override
-	public Dataset transform(Dataset<?> data) {
-	   return data.withColumn("power", functions.pow(data.col(this.column), this.power));
-	}
-	
-	@Override
-	public StructType transformSchema(StructType arg0) {
-	   return arg0;
-	}
+    private final static String uid = Tokenizer.class.getSimpleName() + "_" + UUID.randomUUID().toString();
+
+    private static Map<String, String> stringReplaceMap;
+
+    @Override
+    public void validateInputType(DataType inputType) {
+        assert (inputType.equals(DataTypes.StringType)) :
+                String.format("Input type must be %s, but got %s", DataTypes.StringType.simpleString(), inputType.simpleString());
+    }
+
+    public Function1<String, List<String>> createTransformFunc() {
+        Function1<String, List<String>> f = new TokenizerFunction();
+        return f;
+    }
+
+    public DataType outputDataType() {
+        return DataTypes.createArrayType(DataTypes.StringType, true);
+    }
+
+    public String uid() {
+        return uid;
+    }
+
+    private class TokenizerFunction extends AbstractFunction1<String, List<String>> implements Serializable {
+        public List<String> apply(String sentence) {
+             ... code goes here
+        }
+    }
+
 }
